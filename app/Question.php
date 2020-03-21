0804664c9a3eb0bf750a07cba9a8b03c090b97bf
Abbdurahman Yarow <yarow@Abbdurahmans-MacBook-Pro.local>
2020-03-21 19:54:55 +0100

    Generate Question model and User relations with Question model

---
+++ app/Question.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitelAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}

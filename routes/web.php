b8d0964dab4d2ab23fabc4b172387d34dfed613d
yarowa <eng.yarow@gmail.com>
2020-03-24 09:40:35 +0100

    Created users answer form

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('questions', 'QuestionsController')->except('show');
+//Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
+Route::resource('/questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
 Route::get('/questions/{question:slug}', 'QuestionsController@show')->name('questions.show');

1c0bc5edc13d773fa6edc3e1e48aa137d71d4b0c
yarowa <eng.yarow@gmail.com>
2020-04-10 12:52:15 +0200

    Display user's posts page component

--- resources/js/components/pages/UsersPostsPage.vue
+++ resources/js/components/pages/UsersPostsPage.vue
@@ -1,13 +1,96 @@
 <template>
-    <h1>User's Posts page</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs">
+                            <li class="nav-item">
+                                <router-link exact :to="{name: 'my-posts'}" class="nav-link">All</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link exact :to="{name: 'my-posts', query: { type: 'questions' }}" class="nav-link">Questions</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link exact :to="{name: 'my-posts', query: { type: 'answers' }}" class="nav-link">Answers</router-link>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush" v-if="posts.length">
+                            <li class="list-group-item" v-for="(post, index) in posts" :key="index">
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="post-badge" :class="{accepted: post.accepted }">{{ post.type }}</span>
+                                        <span class="ml-4 votes-count" :class="{accepted: post.accepted }">{{ post.votes_count }}</span>
+                                    </div>
+                                    <div class="col-md-9 text-left">{{ post.title }}</div>
+                                    <div class="col text-right">{{ post.created_at }}</div>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="alert alert-warning" v-else>
+                            <p>You have no any questions or answers</p>
+                            <p>
+                                <router-link :to="{name: 'questions.create'}">Ask question</router-link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "UsersPostsPage"
+        name: "UsersPostsPage",
+        data () {
+            return {
+                posts: []
+            }
+        },
+        mounted() {
+            this.fetchPosts();
+        },
+        methods: {
+            fetchPosts ()  {
+                axios.get('/my-posts', {params: this.$route.query})
+                    .then(({ data }) => {
+                        this.posts = data.data
+                    }).catch(error => console.log(error))
+            }
+        },
+        watch: {
+            '$route': 'fetchPosts'
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    $color-green: rgb(65, 187, 126);
+    .votes-count{
+        border: 1px solid #ddd;
+        display: inline-block;
+        min-width: 40px;
+        text-align: center;
+        &.accepted{
+            background: $color-green;
+            border: 1px solid $color-green;
+            color: #ffffff;
+        }
+    }
 
+    .post-badge{
+        border: 1px solid #ddd;
+        display: inline-block;
+        width: 25px;
+        text-align: center;
+        border-radius: 100%;
+        &.accepted{
+            border: 1px solid $color-green;
+            color: $color-green;
+        }
+    }
 </style>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
@@ -12,10 +12,10 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
     if (to.matched.some(r => r.meta.requiresAuth) && !window.Auth.signedIn) {
-        window.location = window.Urls.login
+        window.location = window.Urls.login;
         return
     }
     next()
-})
+});
 
 export default router
